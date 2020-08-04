a35f47965de28f4e9b46e9e78024d976e8ab995b
donnaphat-utokyo <donnaphattra@g.ecc.u-tokyo.ac.jp>
2020-08-05 01:04:59 +0900

    add file stage-2 file and add make dirs code

---
+++ main_stage_1.py
@@ -0,0 +1,270 @@
+# -*- coding: utf-8 -*-
+import torch
+from dataset import BirdDataset
+from torchvision import transforms
+from torchvision.utils import make_grid
+
+import torch.nn as nn
+import torch.optim as optim
+from torch.utils.data import DataLoader
+
+import numpy as np
+import matplotlib
+matplotlib.use('agg')
+import matplotlib.pyplot as plt
+
+def weights_init(m):
+    classname = m.__class__.__name__
+    if classname.find('Conv') != -1:
+        nn.init.normal_(m.weight.data, 0.0, 0.02)
+    elif classname.find('BatchNorm') != -1:
+        nn.init.normal_(m.weight.data, 1.0, 0.02)
+        nn.init.constant_(m.bias.data, 0)
+        
+CUDA = True
+cond_dim = 128
+df_dim = 128
+gf_dim = 128
+z_dim = 100
+emb_dim = 1024
+
+def Conv_k3(in_p, out_p, stride=1):
+    return nn.Conv2d(in_p, out_p, kernel_size=3, stride=stride, padding=1, bias=False)
+
+class Upblock(nn.Module):
+    def __init__(self, inp, outp):
+        super(Upblock, self).__init__()
+        self.up = nn.Upsample(scale_factor=2, mode='nearest')
+        self.conv = Conv_k3(inp, outp)
+        self.batch = nn.BatchNorm2d(outp)
+        self.relu = nn.ReLU(True)
+        
+    def forward(self, x):
+        o = self.up(x)
+        o = self.relu(self.conv(o))
+        o = self.batch(o)
+        return o
+
+class D_output(nn.Module):
+    def __init__(self, have_cond = True):
+        super(D_output, self).__init__()
+        self.have_cond = have_cond
+        self.classifier = nn.Sequential(
+            nn.Conv2d(in_channels=1024, out_channels=1, kernel_size=4, stride=4),
+            nn.Sigmoid()
+        )
+        if have_cond:
+            cond_part = nn.Sequential(
+                Conv_k3(in_p=1024+128, out_p=1024),
+                nn.BatchNorm2d(1024),
+                nn.LeakyReLU(0.2, inplace=True),
+            )
+            self.classifier = torch.nn.Sequential(*(list(cond_part)+list(self.classifier)))
+        print(self.classifier)
+            
+    def forward(self, encoded_image, encoded_cond=None):
+        if self.have_cond and encoded_cond is not None:
+            cond = encoded_cond.view(-1, 128 , 1, 1)
+            cond = cond.repeat(1, 1, 4, 4)
+            image_with_cond = torch.cat((encoded_image, cond), 1)
+        else:
+            image_with_cond = encoded_image
+        return self.classifier(image_with_cond).view(-1)
+
+class CondAugment_Model(nn.Module):
+    def __init__(self):
+        super(CondAugment_Model,self).__init__()
+        self.fc = nn.Linear(in_features=emb_dim, out_features=cond_dim*2)
+        self.relu = nn.ReLU(True)
+        
+    def convert(self, embed):
+        x = self.relu(self.fc(embed))
+        mean, sigma = x[:, : cond_dim], x[:, cond_dim:]
+        return mean, sigma
+    
+    def forward(self, x):
+        mean, sigma = self.convert(x)
+        diag = torch.exp(sigma*0.5)
+        if CUDA:
+            normal_dis = (torch.FloatTensor(diag.size()).normal_()).cuda()
+        else:
+            normal_dis = (torch.FloatTensor(diag.size()).normal_())
+        condition = (diag*normal_dis)+mean
+        return condition, mean, sigma
+        
+class G_Stage1(nn.Module):
+    def __init__(self):
+        super(G_Stage1, self).__init__()
+        self.CA = CondAugment_Model()
+        self.fc = nn.Sequential(
+            nn.Linear(in_features=228, out_features=128*8*4*4, bias=False),
+            nn.BatchNorm1d(128*8*4*4),
+            nn.ReLU(inplace=True)
+        )
+        self.img = nn.Sequential(
+            Upblock(128*8,64*8),
+            Upblock(64*8,32*8),
+            Upblock(32*8,16*8),
+            Upblock(16*8,8*8),
+            Conv_k3(8*8, 3),
+            nn.Tanh()
+        )
+        
+    def forward(self, noise, emb):
+        cond, mean, sigma = self.CA(emb)
+        cond = cond.view(noise.size(0), cond_dim, 1, 1)
+        x = torch.cat((noise, cond),1)
+        x = x.view(-1, 228)
+        o = self.fc(x)
+        h_code = o.view(-1, 128*8, 4, 4)
+        fake_img = self.img(h_code)
+        return fake_img, mean, sigma
+
+class D_Stage1(nn.Module):
+    def __init__(self):
+        super(D_Stage1, self).__init__()
+        self.encoder = nn.Sequential(
+            #c alucalation output size = [(input_size −Kernal +2Padding )/Stride ]+1
+            # input is image 3 x 64 x 64  
+            nn.Conv2d(in_channels=3, out_channels=128, kernel_size=4, stride=2, padding=1, bias=False),
+            nn.LeakyReLU(0.2, inplace=True),# => 128 x 32 x 32 
+            
+            nn.Conv2d(in_channels=128, out_channels=256, kernel_size=4, stride=2, padding=1, bias=False),
+            nn.BatchNorm2d(256),
+            nn.LeakyReLU(0.2, inplace=True),# => 256 x 16 x 16
+            
+            nn.Conv2d(in_channels=256, out_channels=512, kernel_size=4, stride=2, padding=1, bias=False),
+            nn.BatchNorm2d(512),
+            nn.LeakyReLU(0.2, inplace=True),# => 512 x 8 x 8
+            
+            nn.Conv2d(in_channels=512, out_channels=1024, kernel_size=4, stride=2, padding=1, bias=False),
+            nn.BatchNorm2d(1024),
+            nn.LeakyReLU(0.2, inplace=True)# => 1024 x 4 x 4
+        )
+        self.condition_classifier = D_output()
+        self.uncondition_classifier = None
+        
+    def forward(self, image):
+        return self.encoder(image)
+    
+def KL_loss(mean, sigma):
+        temp = 1+sigma+((-1)*((mean*mean)+sigma))
+        return torch.mean(temp)*(-0.5)
+
+def cal_G_loss(netD, fake_imgs, real_labels, cond):
+    criterion = nn.BCELoss()
+    cond = cond.detach()
+    fake_f = netD(fake_imgs)
+
+    fake_cond_ouput = netD.condition_classifier(fake_f, cond)
+    errD_fake = criterion(fake_cond_ouput, real_labels)
+    if netD.uncondition_classifier is not None:
+        fake_uncond_output = netD.uncondition_classifier(fake_f)
+        uncond_errD_fake = criterion(fake_uncond_output, real_labels)
+        errD_fake += uncond_errD_fake
+    return errD_fake
+
+def cal_D_loss(netD, real_imgs, fake_imgs, real_labels, fake_labels, cond):
+    criterion = nn.BCELoss()
+    batch_size = real_imgs.size(0)
+    cond = cond.detach()
+    fake = fake_imgs.detach()
+
+    real_img_feature = netD(real_imgs)
+    fake_img_feature = netD(fake)
+
+    real_output = netD.condition_classifier(real_img_feature, cond)
+    errD_real  = criterion(real_output, real_labels)
+    wrong_output = netD.condition_classifier(real_img_feature[:(batch_size-1)], cond[1:])
+    errD_wrong = criterion(wrong_output, fake_labels[1:])
+
+    fake_output = netD.condition_classifier(fake_img_feature, cond)
+    errD_fake= criterion(fake_output, fake_labels)
+
+    if netD.uncondition_classifier is not None:
+        real_uncond_output = netD.uncondition_classifier(real_img_feature)
+        errD_real_uncond = criterion(real_uncond_output, real_labels)
+
+        fake_uncond_output = netD.uncondition_classifier(fake_img_feature)
+        errD_fake_uncond = criterion(fake_uncond_output, fake_labels)
+
+        errD = (errD_real+errD_real_uncond)/2. + (errD_fake+errD_wrong+errD_fake_uncond)/3.
+        errD_real =  (errD_real+errD_real_uncond)/2
+        errD_fake = (errD_fake+errD_fake_uncond)/2.
+    else:
+        errD = errD_real + (errD_fake+errD_wrong)*0.5
+    return errD, errD_real.item(), errD_wrong.item(), errD_fake.item()
+    
+def main():
+    device = 'cuda' if torch.cuda.is_available() else 'cpu'
+    batch_size = 64
+    transform = transforms.Compose([
+                transforms.RandomCrop(64),
+                transforms.RandomHorizontalFlip(),
+                transforms.ToTensor(),
+                transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5))])
+
+    dataset = BirdDataset(dataDir = './data/bird_stack/', split='train', transform=transform)
+    tr_loader = DataLoader(dataset, batch_size=batch_size, shuffle=True, drop_last=True)
+    netG = G_Stage1().to(device)
+    netG.apply(weights_init)
+    netD = D_Stage1().to(device)
+    netD.apply(weights_init)
+    lr = 0.0002
+    optD = optim.Adam(netD.parameters(), lr=lr, betas=(0.5, 0.999))
+    optG = optim.Adam(netG.parameters(), lr=lr, betas=(0.5, 0.999))
+
+    fixed_noise = torch.rand(batch_size, z_dim, 1, 1).to(device)
+
+    real_labels = (torch.FloatTensor(batch_size).fill_(1)).to(device)
+    fake_labels = (torch.FloatTensor(batch_size).fill_(0)).to(device)
+    
+    if not (os.path.isdir('./Result_stage1/')):
+        os.makedirs(imageDir)
+    num_epoch = 600
+    iters = 0
+    for epoch in range(num_epoch):
+        if epoch % 100 == 0 and epoch > 0:
+            lr = lr*0.5
+            for param_group in optG.param_groups:
+                param_group['lr'] = lr
+            for param_group in optD.param_groups:
+                param_group['lr'] = lr
+        for i, data in enumerate(tr_loader,0):
+            real_imgs, encoded_caps = data
+            real_imgs = real_imgs.to(device)
+            encoded_caps = encoded_caps.to(device)
+
+            ##update discriminator
+            netD.zero_grad()
+            # generate fake image
+            noise = torch.rand(batch_size, z_dim, 1, 1).to(device)
+            fake_imgs, m, s = netG(noise, encoded_caps)
+            errD, errD_real, errD_wrong, errD_fake = cal_D_loss(netD, real_imgs, fake_imgs, real_labels, fake_labels, m)
+            errD.backward()
+            optD.step()
+
+            ##update generator
+            netG.zero_grad()
+            errG = cal_G_loss(netD, fake_imgs, real_labels, m)
+            errG += errG + KL_loss(m,s)
+            errG.backward()
+            optG.step()
+
+            if i%50 == 0:
+                 print('[%d/%d][%d/%d]\tLoss_D: %.4f\tLoss_G: %.4f\tLoss_D_R: %.4f\tLoss_D_W: %.4f\tLoss_D_F %.4f'
+                      % (epoch, num_epoch, i, len(tr_loader),
+                         errD.item(), errG.item(), errD_real, errD_wrong, errD_fake))
+        if epoch%50==0:
+            with torch.no_grad():
+                fake, _, _  = netG(fixed_noise, encoded_caps)
+                fig = plt.figure(figsize=(10,10))
+                grid = make_grid(fake.detach().cpu(), nrow=8, normalize=True).permute(1,2,0).numpy()
+                plt.imshow(grid)
+                fig.savefig('./Result_stage1/epch-{}.png'.format(epoch))
+
+    torch.save(netG.state_dict(), './Result_stage1/netG_epoch_600.pth')
+    torch.save(netD.state_dict(), './Result_stage1/netD_epoch_last.pth')
+    
+if __name__ == '__main__':
+    main()
